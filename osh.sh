17cdb91ad9b6843c81ba50538a57adec825e85a7
Evan Foss <evanfoss@gmail.com>
2014-07-07 17:12:10 -0500

    Now with BOM (Bill Of Matterials).

---
+++ osh.sh
@@ -0,0 +1,11 @@
+rm backplane-art.fab.gbr
+mv backplane-art.bottom.gbr backplane-art.gbl
+mv backplane-art.bottommask.gbr backplane-art.gbs
+mv backplane-art.bottomsilk.gbr backplane-art.gbo
+mv backplane-art.outline.gbr backplane-art.gko
+mv backplane-art.plated-drill.cnc backplane-art.xln
+mv backplane-art.top.gbr backplane-art.gtl
+mv backplane-art.topmask.gbr backplane-art.gts
+mv backplane-art.topsilk.gbr backplane-art.gto
+
+
